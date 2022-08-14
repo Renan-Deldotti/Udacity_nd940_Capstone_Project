aa852d6719cb8ccea0ed36c01c5561b1b015566d
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2022-08-14 14:54:13 -0300

    Add MoviesListRecyclerView

---
+++ app/src/main/java/com/test/watched/ui/movieslist/MoviesListAdapter.kt
@@ -0,0 +1,70 @@
+package com.test.watched.ui.movieslist
+
+import android.text.TextUtils
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.test.watched.R
+import com.test.watched.data.datamodels.ShortMovieInfo
+import com.test.watched.databinding.SingleMovieItemBinding
+import com.test.watched.utils.Constants
+
+class MoviesListAdapter(private val listener: MoviesListItemListener): ListAdapter<ShortMovieInfo, MoviesListAdapter.ShortMovieInfoItemViewHolder>(DiffCallBack) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShortMovieInfoItemViewHolder {
+        return ShortMovieInfoItemViewHolder(
+            SingleMovieItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ShortMovieInfoItemViewHolder, position: Int) {
+        val movieShortInfo = getItem(position)
+        holder.itemView.setOnClickListener {
+            listener.onMovieClicked(movieShortInfo)
+        }
+        holder.bind(movieShortInfo)
+    }
+
+    inner class ShortMovieInfoItemViewHolder (private val itemBinding: SingleMovieItemBinding): RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(shortMovieInfo: ShortMovieInfo) {
+            itemBinding.smiTitlePreview.text = shortMovieInfo.originalTitle ?: "No title"
+            itemBinding.smiReleaseDatePreview.text = shortMovieInfo.releaseDate ?: "No date"
+            val imagePath:String = Constants.API_IMAGE_BASE_URL + shortMovieInfo.backdropPath
+            if (Constants.API_IMAGE_BASE_URL != shortMovieInfo.backdropPath) {
+                Glide.with(itemBinding.smiPosterPreview)
+                    .load(imagePath)
+                    .placeholder(R.drawable.ic_movies)
+                    .into(itemBinding.smiPosterPreview)
+            } else {
+                Log.d("MoviesListAdapter", "bind: backdropPath is empty, using placeholder image")
+                itemBinding.smiPosterPreview.setImageDrawable(
+                    AppCompatResources.getDrawable(itemBinding.smiPosterPreview.context, R.drawable.ic_movies)
+                )
+            }
+        }
+    }
+
+    companion object DiffCallBack: DiffUtil.ItemCallback<ShortMovieInfo>(){
+        override fun areItemsTheSame(oldItem: ShortMovieInfo, newItem: ShortMovieInfo): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: ShortMovieInfo, newItem: ShortMovieInfo): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    class MoviesListItemListener(val listener: (shortMovieInfo: ShortMovieInfo) -> Unit) {
+        fun onMovieClicked (shortMovieInfo: ShortMovieInfo) = listener(shortMovieInfo)
+    }
+}
--- app/src/main/java/com/test/watched/ui/movieslist/MoviesListFragment.kt
+++ app/src/main/java/com/test/watched/ui/movieslist/MoviesListFragment.kt
@@ -1,21 +1,48 @@
 package com.test.watched.ui.movieslist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.test.watched.R
+import com.test.watched.data.retrofit.RetrofitInstance
+import com.test.watched.databinding.FragmentMoviesListBinding
 
 /**
- * Fragment containig a RecyclerView that displays a list of Moview
+ * Fragment containing a RecyclerView that displays a list of Moview
  */
 class MoviesListFragment : Fragment() {
+
+    private lateinit var binding: FragmentMoviesListBinding
+    private lateinit var moviesListAdapter: MoviesListAdapter
+
+    companion object {
+        private const val TAG = "MoviesListFragment"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+    ): View {
+        binding = FragmentMoviesListBinding.inflate(inflater, container, false)
+
+        moviesListAdapter = MoviesListAdapter(MoviesListAdapter.MoviesListItemListener {
+            Log.d(TAG, "onCreateView: should move to ${it.originalTitle}")
+        })
+
+        binding.moviesListRecyclerView.adapter = moviesListAdapter
+
+        lifecycleScope.launchWhenCreated { testAdapter() }
+
+        return binding.root
+    }
+
+    private suspend fun testAdapter() {
+        val t = RetrofitInstance.api.getPopularMovies(page = "1")
+        Log.d(TAG, "onCreateView: testAdapter ${t.results}")
+        moviesListAdapter.submitList(t.results)
     }
 }
